8e8f7912700afba70476ed2d641767ee9707197d
Don Kaijuka <donald.kaijuka1@gmail.com>
2022-10-18 19:15:18 +0300

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -4,13 +4,13 @@ $email = $_POST['email'];
 $date = $_POST['date'];
 $gender = $_POST['gender'];
 $country = $_POST['country'];
-$user = fopen("user.csv", "w") or die("Unable to open file!");
+
 
 if (empty($_POST["submit"])) {
     echo "Could not insert data!";
 } else {
     $file = fopen("user_data.csv", "a");
-    $srt="Name:".$name."~Email:".$email . "~Date of Birth:".$date ."~Gender:".$gender ."~Country:".$country;// create your string 
+    $srt="Name:".$name."~Email:".$email . "~Date of Birth:".$date ."~Gender:".$gender ."~Country:".$country;// creating string
     echo fwrite($file, $srt);
     fclose($file);
 }
@@ -21,10 +21,7 @@ print_r("<br> Email: " . $email);
 print_r("<br> Date of Birth: " . $date);
 print_r("<br> Country: " . $country);
 print_r("<br> Gender: " . $gender);
-fwrite($user, $name);
-fwrite($user, $date);
-fwrite($user, $gender);
-fclose($user);
+
 }else {
     die("Unable to display");
 }
